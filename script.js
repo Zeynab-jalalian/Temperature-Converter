aabe58a2b333364b78b6250cd6ce8810eb9e666c
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-07-03 16:23:22 +0330

    js

---
+++ script.js
@@ -0,0 +1,22 @@
+const celsius = document.querySelector("#celsius"),
+    fahrenheit = document.querySelector("#fahrenheit");
+
+window.addEventListener("load",()=>{
+    celsius.focus();
+})
+
+celsius.addEventListener("input",()=>{
+    fahrenheit.value=((celsius.value * 9) / 5 + 32).toFixed(2);
+
+    if(!celsius.value){
+        fahrenheit.value="";
+    }
+})
+
+fahrenheit.addEventListener("input",()=>{
+    celsius.value=((celsius.value - 32) * 5 / 9).toFixed(2);
+
+    if(!fahrenheit.value){
+        celsius.value="";
+    }
+})
